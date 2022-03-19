666d8768c51e4503e95c795125b5ebeb10a59fdc
TemuujinTMJ <55456339+TemuujinTMJ@users.noreply.github.com>
2022-03-20 05:52:06 +0800

    another

--- ERC20_Token/extensions/ERC20Pausable.sol
+++ ERC20_Token/extensions/ERC20Pausable.sol
@@ -3,21 +3,8 @@ pragma solidity ^0.8.0;
 import "../ERC20.sol";
 import "../../utils/Pausable.sol";
 
-/**
- * @dev ERC20 token with pausable token transfers, minting and burning.
- *
- * Useful for scenarios such as preventing trades until the end of an evaluation
- * period, or having an emergency switch for freezing all token transfers in the
- * event of a large bug.
- */
 abstract contract ERC20Pausable is ERC20, Pausable {
-    /**
-     * @dev See {ERC20-_beforeTokenTransfer}.
-     *
-     * Requirements:
-     *
-     * - the contract must not be paused.
-     */
+
     function _beforeTokenTransfer(
         address from,
         address to,
